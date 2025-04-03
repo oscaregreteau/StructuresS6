559cefbb960755f5598b0ab929c945407283f527
Oscar Egreteau <oscar.egreteau@icloud.com>
2025-04-03 16:11:40 +0200

    Fin de la séance 2

--- Seance 2/exercice2.py
+++ Seance 2/exercice2.py
@@ -1,5 +1,7 @@
 class Polyq:
     def __init__(self,coef,q,n):
+        self.q=q
+        self.n=n
         self.coef=coef[:]
         for k in range(len(coef)):
             for j in range(1,int(((len(coef)-1)-k)/n)+1):
@@ -21,21 +23,42 @@ class Polyq:
                 m+=str(abs(self.coef[i]))+'*X^'+str(i)+signe(self.coef[i+1])
         return m+str(self.coef[len(self.coef)-1])+'*X^'+str(len(self.coef)-1)
 
+
+    def signe(x):
+        if x<0:
+            return "-"
+        else:
+            return "+"
+
+
+    def scalar(c,P):
+        for i in range(len(P.coef)):
+            P.coef[i]=c*P.coef[i]
+        return P
+
+    def rescale(self,r):
+        Q=self.coef
+        for i in range(len(Q)):
+            Q[i]=Q[i]%r
+        return Polyq(Q)
+
     def __add__(self,P2):
         Q=[0]*(min(len(self.coef),len(P2.coef)))
         for i in range(min(len(self.coef),len(P2.coef))):
             Q[i]=self.coef[i]+P2.coef[i]
         if len(self.coef)>=len(P2.coef):
-            return Polynomial(Q+self.coef[(len(self.coef)-len(P2.coef))+1:])
+            return Polyq(Q+self.coef[(len(self.coef)-len(P2.coef))+1:],self.q,self.n)
         elif len(self.coef)<=len(P2.coef):
-            return Polynomial(Q+P2.coef[(len(P2.coef)-len(self.coef))+1:])
+            return Polyq(Q+P2.coef[(len(P2.coef)-len(self.coef))+1:],self.q,self.n)
         else:
-            return Polynomial(Q)
-
-def signe(x):
-    if x<0:
-        return "-"
-    else:
-        return "+"
+            return Polyq(Q,self.q,self.n)
 
 
+    def __mul__(self,P):
+        Q=[0]*len(self.coef)
+        for i in range(len(self.coef)):
+            for k in range(i):
+                Q[i]+=self.coef[k]*self.coef[i-k]
+        return Polyq(Q,self.n,self.q)
+
+    def karatsuba(self,P):
--- Seance 2/exercice3.py
+++ Seance 2/exercice3.py
@@ -1,6 +1,12 @@
 class Polynomial:
-    def __init__(self,coef):
-        self.coef=coef
+    def __init__(self,coef,q,n):
+        self.coef=coef[:]
+        for k in range(len(coef)):
+            for j in range(1,int(((len(coef)-1)-k)/n)+1):
+                coef[k]+=((-1)**(j))*coef[k+j*n]
+        for j in range(n,len(coef)):
+            coef[j]=0
+        self.coef = [c%q for c in coef]
 
     def __str__(self):
         if self.coef[0]!=0:
@@ -39,15 +45,6 @@ def signe(x):
 #     P3=P1.add(P2)
 #     assert P1.add(P2).coef==P3.coef
 
-    def scalar(c,P):
-        for i in range(len(P.coef)):
-            P.coef[i]=c*P.coef[i]
-        return P
 
-    def rescale(self,r):
-        Q=self.coef
-        for i in range(len(Q)):
-            Q[i]=Q[i]%r
-        return Polynomial(Q)
 
 
